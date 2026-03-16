8deac3946fe32ee77a51682625ea87c9bdcce35b
T-King-00 <tonyshereen@outlook.com>
2026-03-16 12:15:42 +0100

    Ex4 is added,solved.

---
+++ Methods/Ex4/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+string [] randomWords=
+[
+   
+    "cat",
+    "apple",
+    "mountain",
+    "hi",
+    "sun",
+    "television",
+    "a",
+    "tree"
+];
+
+int length1= FindLastLongestStringLength(randomWords);
+int length2=FindLastLongestStringLengthMethod2(randomWords);
+Console.WriteLine("longest string length by method 1 is: "+ length1);
+Console.WriteLine($"longest string length by method 2 is: {length2}");
+
+int FindLastLongestStringLength(string []randomWords)
+{
+    string longestString="";
+    foreach (var word in randomWords)
+    {
+        if (longestString.Length < word.Length)
+        {
+            longestString = word;
+        }
+    }
+    //Console.WriteLine("Longest string is: "+ longestString + " & length of it is "+ longestString.Length);
+    return longestString.Length;
+}
+
+int FindLastLongestStringLengthMethod2(string []randomWords)
+{
+    string longestString="";
+    longestString=randomWords.OrderByDescending(x=>x.Length).First();
+    //Console.WriteLine("Method 2, Longest string is: "+ longestString + " & length of it is "+ longestString.Length);
+    return longestString.Length;
+}
