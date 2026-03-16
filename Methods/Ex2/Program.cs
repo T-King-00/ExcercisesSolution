001bb7772563072812f524edce2d178e8b8250cb
T-King-00 <tonyshereen@outlook.com>
2026-03-16 11:34:31 +0100

    ex2 solved

---
+++ Methods/Ex2/Program.cs
@@ -0,0 +1,40 @@
+﻿//Exercise 2 
+
+string[][] users = [["john", "plate"], ["michelle", "bike"], ["lisa",
+    "pencil"]];
+Console.WriteLine("Enter your username:");
+string username = Console.ReadLine();
+Console.WriteLine("Enter your password:");
+string password = Console.ReadLine();
+
+int userMatch = -1;
+userMatch=FindAMatch(username, password);
+
+bool passwordMatch = false;
+if (userMatch > -1)
+{
+    //code changed
+    passwordMatch = string.Equals(password, users[userMatch][1], StringComparison.OrdinalIgnoreCase); ;
+}
+if (passwordMatch)
+{
+    Console.WriteLine("Success");
+}
+else
+{
+    Console.WriteLine("Login failed");
+}
+
+int FindAMatch( string? s, string? password1)
+{
+    for (int i = 0; i < users.Length; i++)
+    {
+        //code changed : if condition
+        if (  string.Equals(users[i][0], username.ToLower(), StringComparison.OrdinalIgnoreCase) )
+        {
+            userMatch = i;
+            return i;
+        }
+    }
+    return -1 ;
+}
