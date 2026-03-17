022b766843e4fa98882a6e4a6926f42e95d0a09a
T-King-00 <tonyshereen@outlook.com>
2026-03-17 15:04:22 +0100

    Exercise 1 added,solved.

---
+++ ClassAndObjects/Ex1/Employee.cs
@@ -0,0 +1,28 @@
+﻿namespace Ex1;
+
+public class Employee
+{
+    decimal salary;
+    // two different constructors , (overloading)
+    public Employee(decimal salary)
+    {
+        this.salary = salary;
+    }
+
+    public Employee()
+    {
+        
+    }
+    
+    
+    public decimal IncomeTax()
+    {
+       
+        return this.salary * 0.3m  * (20-VacationDays())/20m ;
+    }
+
+    private decimal VacationDays()
+    {
+        return 5;
+    }
+}
--- ClassAndObjects/Ex1/Program.cs
+++ ClassAndObjects/Ex1/Program.cs
@@ -1,5 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Ex1;
+
+Employee employee = new (40000);
+decimal tax = employee.IncomeTax();
+
+Console.WriteLine("Taxable  Income: "+ tax);
+
+
+
+
+
+
 
 
