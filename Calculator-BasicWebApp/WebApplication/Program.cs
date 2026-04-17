629a53b4469634819f4e085b23ecfbf258cdc476
T-King-00 <tonyshereen@outlook.com>
2026-04-17 10:53:36 +0200

    Â-alculator project is added to the main web project. e  -edpoints are added and swagger serivce..

--- Calculator-BasicWebApp/WebApplication/Program.cs
+++ Calculator-BasicWebApp/WebApplication/Program.cs
@@ -1,6 +1,35 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using CalculatorProject;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
+
+//builder.WebHost.ConfigureKestrel(o => o.ListenAnyIP(7270,options  => options.UseHttps()) );
+
+/*
+builder.WebHost.UseIIS();
+builder.WebHost.UseKestrel();
+*/
+
+var app= builder.Build();
+// route for http get request. lambda function is executed when called.
+app.MapGet("/", () => "Calculator Application");
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+CalculatorOperations calculatorOperations = new();
+
+//route for calculator app
+app.MapGet("/add",(double value1,double value2)=> calculatorOperations.Add(value1,value2) );
+app.MapGet("/subtract",(double value1,double value2)=> calculatorOperations.Subtract( value1,value2) );
+app.MapGet("/multiply",(double value1,double value2)=> calculatorOperations.Multiply( value1,value2) );
+app.MapGet("/divide",(double value1,double value2)=> calculatorOperations.Divide(value1, value2));
+
 
 app.Run();
